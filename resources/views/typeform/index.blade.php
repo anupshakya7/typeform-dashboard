b0d8926ab2d4e9e6336179657146224f6437af7a
Anup Shakya <dev2@krizmatic.com>
2025-02-23 15:19:54 +0545

    Typeform Dashboard

---
+++ resources/views/typeform/index.blade.php
@@ -0,0 +1,4 @@
+@extends('typeform.layout.web')
+@section('content')
+<h2>Hello World</h2>
+@endsection
---
+++ storage/framework/views/151609904bd46bb518589a8f974e9a7f.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+<h2>Hello World</h2>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('typeform.layout.web', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/prateeklalwani/Desktop/laravel-template-projects/Laravel/default/resources/views/typeform/index.blade.php ENDPATH**/ ?>
