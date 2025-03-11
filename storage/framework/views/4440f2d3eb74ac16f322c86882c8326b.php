93731746acd6e0ec5547117271a6450b3ada9e12
ArpinGT <arpintamang645@gmail.com>
2025-03-11 10:29:42 +0545

    Last update

--- storage/framework/views/4440f2d3eb74ac16f322c86882c8326b.php
+++ storage/framework/views/4440f2d3eb74ac16f322c86882c8326b.php
@@ -40,9 +40,8 @@
         <div class="flex-shrink-0">
             <div class="d-flex flex-row gap-2 align-items-center">
                 <!--info here-->
-                <button type="button" class="btn btn-success"><i class="ri-file-download-line align-bottom me-1"></i>
-
-                    Export</button>
+                <a href="<?php echo e(route('form.csv')); ?>" type="button" class="btn btn-success"><i class="ri-file-download-line align-bottom me-1"></i>
+                    Export</a>
                 <a class="icon-frame" href="#" class="m-0 p-0 d-flex justify-content-center align-items-center" data-bs-toggle="offcanvas" data-bs-target="#theme-settings-offcanvas"
                 aria-controls="theme-settings-offcanvas">
 
@@ -64,32 +63,35 @@
             </div>
             <div class="card-body">
                 <div class="d-flex flex-row align-items-center justify-content-between pb-3">
+                    <form action="<?php echo e(route('form.index')); ?>" method="GET" id="form_search" style="display: inline-block">
                     <div class="d-flex flex-row align-items-center gap-1">
                         
                     </div>
                     <div class="row">
-                        <div class="col-auto d-flex justify-content-sm-end">
-                            <div class="search-box"> <input type="text" class="form-control" id="searchProductList"
-                                    placeholder="Search"> <i class="ri-search-line search-icon"></i> </div>
-                        </div>
-                        <div class="col-auto"> <select class="form-select " aria-label="Default select example">
-                                <option selected>Country </option>
-                                <option value="1">Australia</option>
-                                <option value="2">USA</option>
-                                <option value="3">India</option>
-                                <option value="4">Nepal</option>
-                            </select> </div>
-                        <div class="col-auto">
-                            <div class="col-auto"> <select class="form-select" aria-label="Default select example">
-                                    <option selected>Project</option>
-                                    <option value="1">Project1</option>
-                                    <option value="2">Project2</option>
-                                    <option value="3">Project3</option>
+                            <div class="col-auto d-flex justify-content-sm-end">
+                                <div class="search-box"> <input type="text" class="form-control" value="<?php echo e(request('search_title')); ?>" name="search_title" onkeyup="debounceSearch()" id="searchProductList"
+                                        placeholder="Search"> <i class="ri-search-line search-icon"></i> </div>
+                            </div>
+                            <div class="col-auto"> 
+                                <select class="form-select select2" name="country" aria-label="Default select example" onchange="this.form.submit()">
+                                    <option value="" selected>Country </option>
+                                    <?php $__currentLoopData = $countries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $country): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <option value="<?php echo e($country['name']); ?>" <?php echo e(request('country') == $country['name'] ? 'selected':''); ?>><?php echo e($country['name']); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                 </select> </div>
-                        </div>
+                            <div class="col-auto">
+                                <div class="col-auto"> 
+                                    <select class="form-select select2" name="organization" aria-label="Default select example" onchange="this.form.submit()">
+                                        <option value="" selected>Organization</option>
+                                        <?php $__currentLoopData = $organizations; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $organization): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <option value="<?php echo e($organization->id); ?>" <?php echo e(request('organization') == $organization->id ? 'selected':''); ?>><?php echo e($organization->name); ?></option>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                    </select> </div>
+                            </div>
                     </div>
+                </form>
                 </div>
-
+            
                 <div class="table-responsive">
                     <table id="scroll-horizontal" class="table nowrap align-middle table-bordered " style="width:100%">
                         <thead class="table-head">
@@ -112,7 +114,7 @@
                             <?php $__currentLoopData = $forms; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key=>$form): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                             <tr>
                                 <th scope="row">
-                                    <?php echo e($key+1); ?>
+                                    <?php echo e($form->serial_no); ?>
 
                                 </th>
                                 <td><?php echo e($form->form_id); ?></td>
@@ -130,18 +132,24 @@
                                             <i class="ri-more-fill align-middle"></i>
                                         </button>
                                         <ul class="dropdown-menu dropdown-menu-end">
-                                            <li><a href="#!" class="dropdown-item"><i
+                                            <li><a href="<?php echo e(route('form.show',$form)); ?>" class="dropdown-item"><i
                                                         class="ri-eye-fill align-bottom me-2 text-muted"></i> View</a>
                                             </li>
-                                            <li><a class="dropdown-item edit-item-btn"><i
+                                            <li><a href="<?php echo e(route('form.question',$form)); ?>" class="dropdown-item"><i class="fa-solid fa-question align-bottom me-2 mb-1 text-muted"></i>Questions</a>
+                                            </li>
+                                            <li><a href="<?php echo e(route('form.edit',$form)); ?>" class="dropdown-item edit-item-btn"><i
                                                         class="ri-pencil-fill align-bottom me-2 text-muted"></i>
                                                     Edit</a>
                                             </li>
                                             <li>
-                                                <a class="dropdown-item remove-item-btn">
-                                                    <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
+                                                <button class="dropdown-item remove-item-btn"
+                                                    data-item-id="<?php echo e($form->id); ?>"
+                                                    data-item-name="<?php echo e($form->form_title); ?>"
+                                                    data-bs-toggle="modal" data-bs-target="#zoomInModal">
+                                                    <i
+                                                        class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
                                                     Delete
-                                                </a>
+                                                </button>
                                             </li>
                                         </ul>
                                     </div>
@@ -152,19 +160,7 @@
                     </table>
                 </div>
                 <!--tfooter section-->
-                <div class="align-items-center mt-xl-3 mt-4 justify-content-between d-flex">
-                    <div class="flex-shrink-0">
-                        <div class="text-muted">Showing <span class="fw-semibold">5</span> of <span
-                                class="fw-semibold">25</span> Results </div>
-                    </div>
-                    <ul class="pagination pagination-separated pagination-sm mb-0">
-                        <li class="page-item disabled"> <a href="#" class="page-link">Previous</a> </li>
-                        <li class="page-item"> <a href="#" class="page-link">1</a> </li>
-                        <li class="page-item active"> <a href="#" class="page-link">2</a> </li>
-                        <li class="page-item"> <a href="#" class="page-link">3</a> </li>
-                        <li class="page-item"> <a href="#" class="page-link">Next</a> </li>
-                    </ul>
-                </div>
+                <?php echo $__env->make('typeform.partials.pagination',['paginator' => $forms], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
             </div>
            
         </div>
@@ -174,130 +170,15 @@
 <!--form section ends here-->
 
 
+ <!-- Modal Blur -->
+ <?php echo $__env->make('typeform.partials.deleteModal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-<div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header bg-light p-3">
-                <h5 class="modal-title" id="exampleModalLabel">&nbsp;</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-                    id="close-modal"></button>
-            </div>
-            <form class="tablelist-form" autocomplete="off">
-                <div class="modal-body">
-                    <input type="hidden" id="id-field" />
 
-                    <div class="mb-3 d-none" id="modal-id">
-                        <label for="applicationId" class="form-label">ID</label>
-                        <input type="text" id="applicationId" class="form-control" placeholder="ID" readonly />
-                    </div>
 
-                    <div class="text-center">
-                        <div class="position-relative d-inline-block">
-                            <div class="position-absolute  bottom-0 end-0">
-                                <label for="companylogo-image-input" class="mb-0" data-bs-toggle="tooltip"
-                                    data-bs-placement="right" title="Select Image">
-                                    <div class="avatar-xs cursor-pointer">
-                                        <div class="avatar-title bg-light border rounded-circle text-muted">
-                                            <i class="ri-image-fill"></i>
-                                        </div>
-                                    </div>
-                                </label>
-                                <input class="form-control d-none" value="" id="companylogo-image-input" type="file"
-                                    accept="image/png, image/gif, image/jpeg">
-                            </div>
-                            <div class="avatar-lg p-1">
-                                <div class="avatar-title bg-light rounded-circle">
-                                    <img src="<?php echo e(URL::asset('build/images/users/multi-user.jpg')); ?>" id="companylogo-img"
-                                        class="avatar-md h-auto rounded-circle object-fit-cover" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="company-field" class="form-label">Company</label>
-                        <input type="text" id="company-field" class="form-control" placeholder="Enter company name"
-                            required />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="designation-field" class="form-label">Designation</label>
-                        <input type="text" id="designation-field" class="form-control" placeholder="Enter designation"
-                            required />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="date-field" class="form-label">Apply Date</label>
-                        <input type="date" id="date-field" class="form-control" data-provider="flatpickr"
-                            data-date-format="d M, Y" required placeholder="Select date" />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="contact-field" class="form-label">Contacts</label>
-                        <input type="text" id="contact-field" class="form-control" placeholder="Enter contact"
-                            required />
-                    </div>
-
-                    <div class="row gy-4 mb-3">
-                        <div class="col-md-6">
-                            <div>
-                                <label for="status-input" class="form-label">Status</label>
-                                <select class="form-control" data-trigger name="status-input" id="status-input">
-                                    <option value="">Status</option>
-                                    <option value="Approved">Approved</option>
-                                    <option value="New">New</option>
-                                    <option value="Pending">Pending</option>
-                                    <option value="Rejected">Rejected</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div>
-                                <label for="type-input" class="form-label">Type</label>
-                                <select class="form-control" data-trigger name="type-input" id="type-input">
-                                    <option value="">Select Type</option>
-                                    <option value="Full Time">Full Time</option>
-                                    <option value="Part Time">Part Time</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <div class="hstack gap-2 justify-content-end">
-                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-success" id="add-btn">Add</button>
-                        
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+                    
 
 <!-- Modal -->
-<div class="modal fade flip" id="deleteOrder" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-body p-5 text-center">
-                <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop"
-                    colors="primary:#405189,secondary:#f06548" style="width:90px;height:90px">
-                </lord-icon>
-                <div class="mt-4 text-center">
-                    <h4>You are about to delete a order ?</h4>
-                    <p class="text-muted fs-15 mb-4">Deleting your order will remove all of your
-                        information from our database.</p>
-                    <div class="hstack gap-2 justify-content-center remove">
-                        <button class="btn btn-link link-success fw-medium text-decoration-none" id="deleteRecord-close"
-                            data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i> Close</button>
-                        <button class="btn btn-danger" id="delete-record">Yes, Delete It</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!--end modal -->
 </div>
 </div>
@@ -343,7 +224,45 @@
 
 <script src="<?php echo e(URL::asset('build/js/pages/datatables.init.js')); ?>"></script>
 
+<script>
+     document.querySelectorAll('.remove-item-btn').forEach(button => {
+            button.addEventListener('click', function() {
+                var itemId = this.getAttribute('data-item-id');
+                var itemName = this.getAttribute('data-item-name');
 
+                //Set the organization Id in the hidden input field
+                document.getElementById('delete_item_id').value = itemId;
+
+                //Set the organization name
+                document.getElementById('delete_item').textContent = itemName;
+
+                //Set Item Description
+                document.getElementById('delete_item_description').innerHTML =
+                    'Deleting this item will permanently remove it from the system, <span class="text-danger"> along with all questions which are part of this form</span';
+
+                var deleteForm = document.getElementById('deleteForm');
+                
+                var currentUrl = window.location.href;
+                        
+                var url = new URL(currentUrl);
+
+                var baseUrl = url.origin+url.pathname;
+
+                deleteForm.action = baseUrl + '/' + itemId;
+
+            })
+        });
+
+        let debounceTimeout;
+
+        function debounceSearch(){
+            clearTimeout(debounceTimeout);
+
+            debounceTimeout = setTimeout(function(){
+                document.getElementById('form_search').submit();
+            },800);
+        }
+</script>
 
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('typeform.layout.web', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\CSB 2025\typeform-dashboard\resources\views/typeform/form/index.blade.php ENDPATH**/ ?>
