eb8ce176ab92279d94bb59dfff0a8423a883229a
satishnidhi <satish.nidhi123@gmail.com>
2025-03-12 13:55:16 +0800

    Remove icons.min.css from tracking

---
+++ storage/framework/views/b0ed54686a2278cddba9c09ed6050b84.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container-fluid text-center">     
+          <p>Copyright © <script>document.write(new Date().getFullYear())</script>- CSB. All Rights Reserved.</p>   
+    </div>
+</footer>
+<?php /**PATH /home/krizmaticcomau/projects.krizmatic.com.au/TypeForm-New/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
