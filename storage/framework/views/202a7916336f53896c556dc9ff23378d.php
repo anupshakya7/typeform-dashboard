601eb3856f89832de1b4976cfc2b94eb799c9b08
ArpinGT <arpintamang645@gmail.com>
2025-03-17 16:11:29 +0545

    Master Merged

---
+++ storage/framework/views/202a7916336f53896c556dc9ff23378d.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container-fluid text-center">     
+          <p>Copyright © <script>document.write(new Date().getFullYear())</script>- CSB. All Rights Reserved.</p>   
+    </div>
+</footer>
+<?php /**PATH F:\CSB 2025\typeform-dashboard\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
