3bdefda2a1330c381f81781c5174d410c49868ac
Anup Shakya <dev2@krizmatic.com>
2025-03-25 12:31:44 +0545

    Update New Design and Form

--- storage/framework/views/cbd5f0a705e3e786380a9e9fa3616f7f.php
+++ storage/framework/views/cbd5f0a705e3e786380a9e9fa3616f7f.php
@@ -1,11 +1,11 @@
 
 <body>
-<!-- <div class="download-spinner-container">
-<div class="d-flex justify-content-center">
+<div class="download-spinner-container">
+<div class="d-flex justify-content-center align-items-center h-100">
   <div class="spinner-border" role="status">
     <span class="visually-hidden">Loading...</span>
   </div>
 </div>
-</div> -->
+</div>
 <!-- <body data-layout="horizontal"> -->
 <?php /**PATH /Users/prateeklalwani/Desktop/Typeform Main/typeform-dashboard/resources/views/layouts/body.blade.php ENDPATH**/ ?>
