77cf807e93aa2027192c1b3cd424eb70dd32b882
ArpinGT <arpintamang645@gmail.com>
2025-03-26 14:51:35 +0545

    Form Submit Modal Updated

--- storage/framework/views/b8e267b3dc6a1c2f1bdd09cee2961a9b.php
+++ storage/framework/views/b8e267b3dc6a1c2f1bdd09cee2961a9b.php
@@ -36,7 +36,7 @@
             </div>
 
             <div class="card-body">
-                <div class="mb-2">
+                <div class="mb-2 table-responsive">
                     <table class="table table-bordered table-striped">
                         <tbody>
                             <tr>
@@ -61,7 +61,7 @@
                             </tr>
                             <tr>
                                 <th>Division</th>
-                                <td><?php echo e($form->branches ? optional($form->branches)->name : 'Main Branch'); ?></td>
+                                <td><?php echo e($form->branches ? optional($form->branches)->name : 'Head Office'); ?></td>
                             </tr>
                             <tr>
                                 <th>Before Date</th>
